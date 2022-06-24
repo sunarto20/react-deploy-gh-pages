37d7407361de11992a57c43624712bdc1e49f864
Sunarto <sunarto.narnia@gmail.com>
2022-06-24 17:51:51 +0700

    Updates

---
+++ precache-manifest.e951bb534f58be032b9eedf31779425f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c88683e4dabd58bd3d4",
+    "url": "/react-deploy-gh-pages/static/css/main.a19025d7.chunk.css"
+  },
+  {
+    "revision": "7c88683e4dabd58bd3d4",
+    "url": "/react-deploy-gh-pages/static/js/main.7c88683e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-deploy-gh-pages/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/react-deploy-gh-pages/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "55774f9efa64a1389d2f",
+    "url": "/react-deploy-gh-pages/static/js/runtime~main.55774f9e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-deploy-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d584bc8383872c807a71542f7fe473f2",
+    "url": "/react-deploy-gh-pages/index.html"
+  }
+];
